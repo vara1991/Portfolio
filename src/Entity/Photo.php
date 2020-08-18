bbfc3715abb1e58008c9f87481a5ecfef23e5930
vara1991 <varaponegaire@gmail.com>
2020-08-18 14:50:15 +0200

    add vichupload

--- src/Entity/Photo.php
+++ src/Entity/Photo.php
@@ -4,9 +4,13 @@ namespace App\Entity;
 
 use App\Repository\PhotoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+
 
 /**
  * @ORM\Entity(repositoryClass=PhotoRepository::class)
+ * @Vich\Uploadable
  */
 class Photo
 {
@@ -23,6 +27,12 @@ class Photo
     private $img;
 
     /**
+     * @Vich\UploadableField(mapping="img_file", fileNameProperty="img")
+     * @var File
+     */
+    private $imgFile;
+    
+    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $name;
@@ -36,6 +46,11 @@ class Photo
      * @ORM\ManyToOne(targetEntity=Project::class, inversedBy="photos")
      */
     private $project;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -52,6 +67,19 @@ class Photo
         $this->img = $img;
 
         return $this;
+    }
+
+    public function setImgFile(File $img = null)
+    {
+        $this->imgFile = $img;
+        if ($img) {
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImgFile(): ?File
+    {
+        return $this->imgFile;
     }
 
     public function getName(): ?string
@@ -93,5 +121,17 @@ class Photo
     public function __toString()
     {
         return $this->name;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
     }
 }
